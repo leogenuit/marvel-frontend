519ae742ce68c2f585936010d5b8d2b0bddc7337
leogenuit <genuitleo@gmail.com>
2024-06-19 17:38:53 +0200

    change dir component directory name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
-import Header from "./component.jsx/Header";
+import Header from "./components/Header";
 const App = () => {
   return (
     <Router>
